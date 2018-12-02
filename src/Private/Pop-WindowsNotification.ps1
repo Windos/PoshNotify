f31d710f642306ef464f41d83f0b59b9d6c5de70
Windos <windosbk+github@gmail.com>
2018-12-03 12:25:59 +1300

    Adding customizable icon

--- src/Private/Pop-WindowsNotification.ps1
+++ src/Private/Pop-WindowsNotification.ps1
@@ -8,7 +8,11 @@ Function Pop-Notification {
 
         [Parameter(Mandatory=$true)]
         [string]
-        $Title
+        $Title,
+
+        [Parameter()]
+        [string]
+        $Icon = "$PSScriptRoot\lib\alarm.png"
     )
 
     $XmlString = @"
@@ -17,19 +21,20 @@ Function Pop-Notification {
         <binding template="ToastGeneric">
         <text>$Title</text>
         <text>$Body</text>
-        <image src="$PSScriptRoot\lib\alarm.png" placement="appLogoOverride" hint-crop="circle" />
+        <image src=$Icon placement="appLogoOverride" hint-crop="circle" />
         </binding>
     </visual>
     <audio src="ms-winsoundevent:Notification.Default" />
     </toast>
 "@
 
-    $AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+    $AppId = Get-WindowsAppId
+
     $null = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
     $null = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
     $ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::new()
     $ToastXml.LoadXml($XmlString)
+
     $Toast = [Windows.UI.Notifications.ToastNotification]::new($ToastXml)
     [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).Show($Toast)
-
 }

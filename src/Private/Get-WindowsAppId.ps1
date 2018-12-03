fa9191d87697e95ee45cdadf62be7098e4bd7015
Windos <windosbk+github@gmail.com>
2018-12-03 16:17:16 +1300

    Remove un-needed $IsWindows

--- src/Private/Get-WindowsAppId.ps1
+++ src/Private/Get-WindowsAppId.ps1
@@ -3,7 +3,7 @@
 function Get-WindowsAppId {
     if ($PSEdition -eq 'Desktop'){
         (Get-StartApps -Name 'PowerShell').where({$_.Name -eq 'Windows PowerShell'}).AppId
-    } elseif ($IsWindows -and $PSVersionTable.PSVersion -ge [Version]'6.1') {
+    } elseif ($PSVersionTable.PSVersion -ge [Version]'6.1') {
         Import-Module StartLayout -SkipEditionCheck
         (Get-StartApps -Name 'PowerShell').where({$_.Name -like 'PowerShell 6*'})[0].AppId
     } else {
